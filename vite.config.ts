82080ef49b5f76da9e482ff335afeb1d1f0f5004
adlong <3140618@163.com>
2023-03-17 14:00:21 +0800

    rm not need package

--- vite.config.ts
+++ vite.config.ts
@@ -13,10 +13,13 @@ export default defineConfig({
 			resolvers: [ElementPlusResolver()]
 		}),
 		Components({
+			// dirs 指定组件所在位置，默认为 src/components
+			// 可以让我们使用自己定义组件的时候免去 import 的麻烦
+			dirs: ['src/components/'],
+
+			// 配置需要将哪些后缀类型的文件进行自动按需引入
+			extensions: ['vue', 'md'],
 			resolvers: [ElementPlusResolver()]
 		})
-	],
-	optimizeDeps: {
-		include: ['schart.js']
-	}
+	]
 });
